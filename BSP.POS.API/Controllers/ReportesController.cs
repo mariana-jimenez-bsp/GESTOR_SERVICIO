b89d2d7f0370d5177ded7c99000392b465260ea0
JoseBsp <jose.sanchez.bsp@gmail.com>
2023-08-31 14:31:28 -0600

    Mejora Api Reportes

--- BSP.POS.API/Controllers/ReportesController.cs
+++ BSP.POS.API/Controllers/ReportesController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BSP.POS.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ReportesController : ControllerBase
     {
         [HttpGet("GenerarReportePorConsecutivo/{esquema}/{consecutivo}")]
