cec7fc76f342acf9d17b9ef17fefbe68e5d24ce5
JoseBsp <jose.sanchez.bsp@gmail.com>
2023-10-17 16:12:14 -0600

    Corrección api reportes

--- BSP.POS.API/Controllers/ReportesController.cs
+++ BSP.POS.API/Controllers/ReportesController.cs
@@ -11,11 +11,12 @@ namespace BSP.POS.API.Controllers
     {
         private readonly string _tipoInicio = string.Empty;
         private readonly string _urlApiCrystal = string.Empty;
-        public ReportesController() {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public ReportesController(IWebHostEnvironment hostingEnvironment) {
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .Build();
-
+            _hostingEnvironment = hostingEnvironment;
             _tipoInicio = configuration["AppSettings:TipoInicio"];
             if (_tipoInicio == "debug")
             {
@@ -32,7 +33,9 @@ namespace BSP.POS.API.Controllers
         {
             try
             {
-                using (HttpClient client = new HttpClient())
+                HttpClientHandler clientHandler = new HttpClientHandler();
+                clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+                using (HttpClient client = new HttpClient(clientHandler))
                 {
                     var response = await client.GetAsync(_urlApiCrystal + "Api/GenerarReporte/" + esquema + "/" + consecutivo);
 
@@ -51,6 +54,15 @@ namespace BSP.POS.API.Controllers
             }
             catch (Exception ex)
             {
+                if (_tipoInicio == "deploy")
+                {
+                    string pathError = Path.Combine(_hostingEnvironment.ContentRootPath, "WhatsappService", "MensajesJson", "TextError.txt");
+                    System.IO.File.WriteAllText(pathError, ex.ToString());
+                    if (ex.InnerException != null)
+                    {
+                        System.IO.File.AppendAllText(pathError, "\nInner Exception: " + ex.InnerException.ToString());
+                    }
+                }
                 return StatusCode(500, $"Error Interno de servidor: {ex.Message}");
             }
         }
