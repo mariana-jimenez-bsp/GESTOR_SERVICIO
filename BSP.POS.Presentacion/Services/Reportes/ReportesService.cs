dc5a02df12db4dd2556cfbaa3b5d695d525de658
JoseBsp <jose.sanchez.bsp@gmail.com>
2023-11-09 19:02:07 -0600

    mejoras para deploy de reportes y licencias

--- BSP.POS.Presentacion/Services/Reportes/ReportesService.cs
+++ BSP.POS.Presentacion/Services/Reportes/ReportesService.cs
@@ -1,4 +1,5 @@
 ﻿using BSP.POS.Presentacion.Interfaces.Reportes;
+using BSP.POS.Presentacion.Pages.Clientes;
 
 namespace BSP.POS.Presentacion.Services.Reportes
 {
@@ -13,15 +14,27 @@ namespace BSP.POS.Presentacion.Services.Reportes
         public async Task<byte[]> GenerarReporteDeInforme(string esquema, string consecutivo)
         {
 
-                var response = await _http.GetAsync($"Reportes/GenerarReportePorConsecutivo/{esquema}/{consecutivo}");
+            //var response = await _http.GetAsync($"Reportes/GenerarReportePorConsecutivo/{esquema}/{consecutivo}");
 
-                if (response.IsSuccessStatusCode)
-                {
-                    return await response.Content.ReadAsByteArrayAsync();
-                }
-                var reponseMessage = response.RequestMessage;
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    return await response.Content.ReadAsByteArrayAsync();
+            //}
+            //var reponseMessage = response.RequestMessage;
+            //throw new Exception("Error al obtener el PDF");
+            var response = await _http.GetAsync("https://cloud.bspcr.com:4443/POS_Prueba_APICrystal_Gestor_servicios/Api/GenerarReporte/" + esquema + "/" + consecutivo);
+            if (response.IsSuccessStatusCode)
+            {
+                var fileBytes = await response.Content.ReadAsByteArrayAsync();
+
+                // Envía los bytes directamente al cliente como un archivo PDF
+                return fileBytes;
+            }
+            else
+            {
                 throw new Exception("Error al obtener el PDF");
-            
+            }
+
         }
     }
 }
